870ca92bc8ef66a419844b2d0ab7ecbc2f8f61c0
Vicky Chawand <shivanshchawand044@gmail.com>
2023-12-06 23:28:18 +0530

    update js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".multiple-text",{
-    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Software Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay: 1000,
